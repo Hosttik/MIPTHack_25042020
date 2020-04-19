66f7c0acedf3a7562590d7fc7d2cf5a0e345a99c
fast93@mail.ru <fast93@mail.ru>
2020-04-19 13:58:42 +0300

    ngrok parser changed

--- scripts/parse_ngrok.py
+++ scripts/parse_ngrok.py
@@ -6,8 +6,13 @@ if __name__ == '__main__':
     response = requests.get('http://127.0.0.1:4040/api/tunnels')
 
     data = response.json()
-#     поправить нахождение корретных url, массив data['tunnels'] оказывается не детерминированный
-    frontend_url = data['tunnels'][1]['public_url'].replace('https', 'http')
-    backend_url = data['tunnels'][0]['public_url'].replace('https', 'http')
+	frontend_url = None
+	
+	backend_url = None
+	for tunnel in data['tunnels']:
+		if tunnel['name'] == 'frontend':
+			frontend_url = tunnel['public_url'].replace('https', 'http')
+		if tunnel['name'] == 'backend':
+			backend_url = tunnel['public_url'].replace('https', 'http')
 
     print("{};{}".format(frontend_url,backend_url))
